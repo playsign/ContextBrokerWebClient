472d8d01f272686b9e8e879bce24dcf5106f9f1d
Toni Alatalo <toni@playsign.net>
2014-11-14 02:31:17 +0200

    use heat data from santander json to populate the heatmap. works but to be adjusted.

--- threejs_heatmap.js
+++ threejs_heatmap.js
@@ -47,15 +47,89 @@ scene.add(cube);
           console.log('took ', (+new Date) - init, 'ms');
         };
         // initial generate
-        generate();
+        //generate();
+updateData();
 
-        // whenever a user clicks on the ContainerWrapper the data will be regenerated -> new max & min
-        document.getElementById('heatmapContainerWrapper').onclick = function() { 
-            generate(); 
-            cube.material.map.needsUpdate = true;            
+//sensor data reading -- instead of the random generation from example (above)
+function updateData() {
+    // COPY-PASTE from tapanij-vizi-poi Scene.js !!!
+    ///Get offline Context Broker info
+    var sceneScope = this;
+    $.getJSON("nodeinfo.json", function(data) {
+	heatmap_parseOfflineCBData(data);
+    });
+}
+
+function normaliseGeopos(geopos) {
+/* the area in Santander
+bottom-left: 43.463369, -3.805923
+top-right:   43.476170, -3.789915
+*/
+    var east = -3.805923;
+    var west = -3.789915;
+    var south = 43.463369;
+    var north = 43.476170;
+
+    var width = west - east;
+    var height = north - south;
+    
+    var scale_x = 1 / width;
+    var scale_y = 1 / height;
+    
+    var x = (west - geopos[1]) * scale_x;
+    var y = (north - geopos[0]) * scale_y;
+
+    return [x, y];
+}
+
+function heatmap_parseOfflineCBData(json) {
+    console.log("hep");
+
+    //read temperatures from own offline json
+    var keys = Object.keys(json);
+    var temperatures = []; //longlat, celciusfloat pairs
+    for (k in keys) { //n for node, as in sensor (in nid, ndata etc)
+        var nid = keys[k];
+        var ninfo = json[nid];
+        var ndata = ninfo.data; //there's also geopos
+        if ("Temperature" in ndata) {
+            var temperinfo = ndata["Temperature"]
+            var tempernum = parseFloat(temperinfo); //temperinfo.slice(0, -2)
+            temperatures.push([ninfo.geopos, tempernum]);
+        }
+    }
+    //console.log(temperatures);
+
+    //populate heatmap data
+    var hmapdata = [];
+    THREE.hmapdata = hmapdata;
+    r = 10;
+    temperatures.forEach(function(t) {
+        //console.log(t);
+        norm_xypos = normaliseGeopos(t[0]);
+        var canvas = heatmap._renderer.canvas;
+        var xypos = [-1, -1];
+        xypos[0] = norm_xypos[0] * canvas.width;
+        xypos[1] = norm_xypos[1] * canvas.height;
+        //console.log(xypos);
+        hmapdata.push({ x: xypos[0], y: xypos[1], value: t[1], radius: r });
+    });
+
+    heatmap.setData({
+        min: 10,
+        max: 30,
+        data: hmapdata
+    });
+}
+
+// whenever a user clicks on the ContainerWrapper the data will be regenerated -> new max & min
+document.getElementById('heatmapContainerWrapper').onclick = function() { 
+    //generate();
+    updateData();
+    cube.material.map.needsUpdate = true;            
 };
 
 var heatmap_tex = new THREE.Texture(heatmap._renderer.canvas, new THREE.UVMapping(), THREE.RepeatWrapping, THREE.RepeatWrapping);
 mat.map = heatmap_tex;
 cube.material.map.needsUpdate = true;
-heatmap._renderer.canvas.parentNode.removeChild(heatmap._renderer.canvas);
+//heatmap._renderer.canvas.parentNode.removeChild(heatmap._renderer.canvas);
